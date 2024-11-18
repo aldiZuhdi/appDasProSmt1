4c606c722244e96b676e6eddb07c29eef7afad51
aldiZuhdi <zuhdiahmadmaulana@gmail.com>
2024-11-18 16:20:56 +0700

    add register fitur

--- register.py
+++ register.py
@@ -1,8 +1,27 @@
-from app import db_connection
+import time
+from app import db_connection, clear
 
 def main(username, password):
-    db = db_connection()
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM users WHERE username = %s AND password = %s", (username, password))
-    get_data = cursor.fetchone()
-    if get_data rowCount
+    try:
+        db = db_connection()
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM users WHERE username = %s AND password = %s", (username, password))
+        cursor.fetchall()
+        db.commit()
+        if cursor.rowcount == 0:
+            cursor.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (username, password))
+            db.commit()
+            print("Akses berhasil ditambahkan !")
+            time.sleep(2)
+            clear()
+        else:
+            print("Username atau password sudah digunakan, silahkan buat yang lain")
+    except ValueError as e:
+        print(f"Error: {e}")
+    finally:
+        db.close()
+        cursor.close()
+                
+username = input("Silahkan buat username: ")
+password = input("Silahkan buat password: ")
+main(username, password)
